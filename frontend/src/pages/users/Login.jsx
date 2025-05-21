04dbdc22709f6722da06e68d475d981376555d81
Belasoftengg <155488654+Belasoftengg@users.noreply.github.com>
2025-05-21 17:19:29 +0300

    sdsfdsfd

--- frontend/src/pages/users/Login.jsx
+++ frontend/src/pages/users/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
     setError("");
   
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://volunteershub-6.onrender.com/api/auth/login", {
         method: "POST",
         headers: { 
           "Content-Type": "application/json",

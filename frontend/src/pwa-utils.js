aee007631dda41169b27d11b2539e0b02256b240
Ephrem Mekonnen <ephrem8118@gmail.com>
2025-05-23 20:12:23 +0300

    Update pwa-utils.js

--- frontend/src/pwa-utils.js
+++ frontend/src/pwa-utils.js
@@ -6,10 +6,7 @@ export function showUpdateNotification(registration) {
   const notification = document.createElement("div")
   notification.className = "update-notification"
   notification.innerHTML = `
-    <div class="update-notification-content">
-      <p>New version available!</p>
-      <button id="update-app">Update now</button>
-    </div>
+    
   `
 
   document.body.appendChild(notification)
